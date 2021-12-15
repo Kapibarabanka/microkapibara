ae0a29f48493fd97174e27d4bc0dfaf64aacbc1a
Kapibarabanka <kapibarabanka@gmail.com>
2021-12-15 18:16:30 +0200

    registry and config work

--- hello-service/src/main/java/com/microkapibara/helloservice/controller/HelloController.java
+++ hello-service/src/main/java/com/microkapibara/helloservice/controller/HelloController.java
@@ -1,10 +1,10 @@
-package com.microkapibara.helloservice;
+package com.microkapibara.helloservice.controller;
 
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
-    @RequestMapping(path = "/", method = RequestMethod.GET)
+    @RequestMapping(path = "/my_hello", method = RequestMethod.GET)
 	public String getHello() {
 		return "HELLO FROM FUCKING SERVICES";
 	}
