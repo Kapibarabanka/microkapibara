34f758174515dcec70f49b7abe3584204fc75d78
Kapibarabanka <kapibarabanka@gmail.com>
2021-12-16 23:27:04 +0200

    python config and balancing

--- hello-service/src/main/java/com/microkapibara/helloservice/HelloController.java
+++ hello-service/src/main/java/com/microkapibara/helloservice/HelloController.java
@@ -13,6 +13,7 @@ public class HelloController {
 	public String getHello() {
 		String greeting = "Hello from java";
 		String updatedGreeting = pyClient.processGreeting(greeting);
+		System.out.println("JAVA_LOG: Java did its job");
 		return updatedGreeting;
 	}
 }
--- py-service/app.py
+++ py-service/app.py
@@ -1,9 +1,19 @@
 from flask import Flask
 import py_eureka_client.eureka_client as eureka_client
+from spring_config import ClientConfigurationBuilder
+from spring_config.client import SpringConfigClient
 
-rest_port = 8082
-eureka_client.init(eureka_server="http://registry:8761/eureka",
-                   app_name="py-service",
+service_name = 'py-service'
+
+config = ClientConfigurationBuilder().app_name(service_name).address('http://config:8888').build()
+c = SpringConfigClient(config)
+service_config = c.get_config()
+
+eureka_server = service_config['eureka']['client']['serviceURL']['defaultZone']
+rest_port = service_config['server']['port']
+
+eureka_client.init(eureka_server=eureka_server,
+                   app_name=service_name,
                    instance_port=rest_port)
 
 app = Flask(__name__)
@@ -11,6 +21,7 @@ app = Flask(__name__)
 @app.route("/py-hello/process-hello/<string:greeting>", methods=['GET'])
 def hello(greeting: str):
     greeting += "\nAnd hello from Python"
+    app.logger.info('PYTHON_LOG:', 'Python did its job')
     return greeting
 
 if __name__ == "__main__":
